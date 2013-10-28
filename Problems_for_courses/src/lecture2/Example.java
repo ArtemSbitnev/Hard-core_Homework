c6844e8608028d77faf0ef954b6d5ef5c72b31dd
ArtemSbitnev <artem.sbitnev@mail.ru>
2013-10-28 22:35:43 +0200

    update

--- Problems_for_courses/src/lecture2/Example.java
+++ Problems_for_courses/src/lecture2/Example.java
@@ -16,37 +16,5 @@ public class Example {
 		firstTree.remove(5);
 		firstTree.printByDepth();
 		firstTree.printByWidth();
-		/*
-		BinTree<Integer> firstTree = new BinTree<Integer>();
-		
-		Integer a = 5;
-		firstTree.add(a);
-		for (int i = 1; i < 8; i ++) {
-			a = i;
-			firstTree.add(i);			
-		}
-		System.out.println(firstTree.exists(16));
-		System.out.println(firstTree.exists(7));
-		
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(5));
-
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(4));
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(10));
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(12));
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(7));
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(8));
-		firstTree.printByWidth();		
-		firstTree.add(8);
-		firstTree.add(7);
-		firstTree.printByDepth();
-		System.out.println(firstTree.remove(7));
-		firstTree.printByDepth();
-		*/
 	}
 }
